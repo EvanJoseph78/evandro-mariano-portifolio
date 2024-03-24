0ccd0ce0384817dd708bb3b9dc07036f6e77ffec
evan <evandromariano49@gmail.com>
2024-03-24 14:50:13 -0300

    feat: side-bar

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -2,20 +2,27 @@ import { Component } from '@angular/core';
 import { DarkmodeService } from '../../../services/darkmode.service';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, SideBarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  slideBar: boolean = false;
+
   constructor(public darkModeService: DarkmodeService) { }
 
   toggleDarkMode() {
     this.darkModeService.toggleDarkMode();
+  }
+
+  toggleSlideBar() {
+    this.slideBar = !this.slideBar;
   }
 
 }
